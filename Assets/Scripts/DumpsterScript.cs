5ec973b0204ae912a39fc14f229743ef5032f8ea
Jonas Costa <38665874+jonasccosta@users.noreply.github.com>
2022-12-09 14:20:39 -0600

    Added audio to game objects

--- Assets/Scripts/DumpsterScript.cs
+++ Assets/Scripts/DumpsterScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class DumpsterScript : MonoBehaviour
 {
+    public AudioSource dumpsterAudio;
+    public AudioSource debriAudio;
+    public Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+         if (rigidBody.velocity.magnitude >= 0.1 && !dumpsterAudio.isPlaying ){
+            dumpsterAudio.Play();
+
+         }
+
+         else if (rigidBody.velocity.magnitude < 0.1){
+            dumpsterAudio.Pause();
+         }
+
         
     }
 
@@ -22,8 +36,14 @@ public class DumpsterScript : MonoBehaviour
     {
         if(transform.parent != null){
             if(transform.parent.tag == "Player" && collision.gameObject.tag == "Obstacle"){
+                debriAudio.Play();
                 Destroy(collision.gameObject);
             }
        }
+
     }
+
+   
+
+    
 }
